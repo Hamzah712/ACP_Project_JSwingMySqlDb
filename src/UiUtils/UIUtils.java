47170f2a8b379efc7ca487525362ab5dbd174edc
Hamzah712 <hamzah.712680678@gmail.com>
2024-05-26 14:43:04 +0500

    Added few classes and db with db connection, I also need to modify the stolen homepage class.

---
+++ src/UiUtils/UIUtils.java
@@ -0,0 +1,29 @@
+package UiUtils;// UIUtils.java
+import javax.swing.*;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class UIUtils {
+
+    // Method to set the frame icon from a file path
+    public static void setFrameIcon(JFrame frame, String filePath) {
+        try {
+            URL iconURL = new File(filePath).toURI().toURL();
+            ImageIcon icon = new ImageIcon(iconURL);
+            frame.setIconImage(icon.getImage());
+        } catch (MalformedURLException e) {
+            System.err.println("Invalid file path: " + filePath);
+            e.printStackTrace();
+        }
+    }
+
+    // Method to set the look and feel
+    public static void setLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
